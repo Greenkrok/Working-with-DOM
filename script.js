4542593cf6c597fdf81200b22c1a377790f5f7f9
Greenkrok <hellsing_13@mail.ru>
2020-11-06 16:28:02 +0300

    feat: closing a modal window and clearing input fields

--- script.js
+++ script.js
@@ -7,11 +7,34 @@
 // Добавим функционал закрытия модального окна.
 // по клику на знак “Х” в правом верхнем углу;
 // по клику на кнопку “close”;
-// по клику на серую область вокруг диалогового окна.
 // При закрытии окна поля ввода должны очищаться.
 
+
+// Открытие окна по клику на кнопку 'add user'
 let $btn = document.getElementsByClassName('add-user')[0];
 let $modal = document.getElementsByClassName('modal')[0];
+let $inputs = document.querySelectorAll('input');
 $btn.addEventListener('click', function () {
     $modal.classList.add('open');
 });
+
+// Закрытие окна по клику на крестик
+let $close = document.getElementsByClassName('close')[0];
+$close.addEventListener('click', function() {
+    $modal.classList.remove('open');
+    // Обнуление полей для ввода
+    for(input of $inputs) {
+        input.value = '';
+    };
+});
+
+// Закрытие окна по клику на 'close'
+let $btnClose = document.getElementsByClassName('btn close')[0];
+$btnClose.addEventListener('click', function() {
+    $modal.classList.remove('open');
+    // Обнуление полей для ввода
+    for(input of $inputs) {
+        input.value = '';
+    };
+});
+
